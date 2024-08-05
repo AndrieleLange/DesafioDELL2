6b9fdb7e71bae48715a2b474b47c74c580175129
Andriele Barcé Lange <115933269+AndrieleLange@users.noreply.github.com>
2024-08-05 10:56:15 -0300

    Delete demo directory

--- demo/src/main/java/com/andrielelange/Main.java
+++
@@ -1,7 +0,0 @@
-package com.andrielelange;
-
-import com.andrielelange.VisualizacaoS;
-
-public class Main {
-    
-}
--- demo/src/main/java/com/andrielelange/view/Main.java
+++
@@ -1,8 +0,0 @@
-package view;
-
-public class Main {
-    public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
-        ui.iniciar();
-    }
-}
